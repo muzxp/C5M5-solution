c843040f1ac47564bcb3ae7902c0131557dcfb4a
Mutsuji Takahashi <muz_@nifty.com>
2020-01-31 07:42:29 +0900

    myinfo/signup(both only template)added

--- src/public/public.routes.js
+++ src/public/public.routes.js
@@ -40,6 +40,23 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
-    });
+    })
+    .state('public.myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/myinfo/myinfo.html'
+      // templateUrl: 'src/public/signup/signup.html'
+      // controller: 'SignupController',
+      // controllerAs: 'signupCtrl',
+      
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html'
+      // controller: 'SignupController',
+      // controllerAs: 'signupCtrl',
+      
+    })
+
+    ;
 }
 })();
